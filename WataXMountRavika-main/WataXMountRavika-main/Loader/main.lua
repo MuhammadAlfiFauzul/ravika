dfeed32cdb3e6869cac5abc4fc8e623de4796b7f
MuhammadAlfiFauzul <alfikerja12@gmail.com>
2025-10-01 00:40:13 +0700

    Update main.lua

--- WataXMountRavika-main/WataXMountRavika-main/Loader/main.lua
+++ WataXMountRavika-main/WataXMountRavika-main/Loader/main.lua
@@ -3,8 +3,6 @@ local Players = game:GetService("Players")
 local StarterGui = game:GetService("StarterGui")
 local player = Players.LocalPlayer
 
-local urlVip = "https://raw.githubusercontent.com/WataXScript/VerifikasiScWata/refs/heads/main/Loader/vip.txt"
-local urlSatuan = "https://raw.githubusercontent.com/WataXScript/VerifikasiScWata/refs/heads/main/Loader/ravika.txt"
 
 local successUrls = {
     "https://raw.githubusercontent.com/WataXScript/WataXMountAtin/main/Loader/WataX.lua",
